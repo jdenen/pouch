ee921e9328c5c833f09c204f2f99c7e30555d711
Johnson Denen <johnson.denen@gmail.com>
2015-08-01 22:54:10 -0400

    Update spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,4 @@
-$LOAD_PATH.unshift(File.expand_path '../../lib', __FILE__)
-
+require 'simplecov'
+SimpleCov.start
 require 'pouch'
 require 'rspec'
-
-RSpec.configure do |config|
-  config.order = 'default'
-end
